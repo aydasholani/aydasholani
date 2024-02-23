793577fcbcb7730a3d9d0b3b387b606895fe91f9
Ayda Sholani <ayda.sholani@gmail.com>
2024-02-23 15:12:49 +0100

    create routes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Ayda Sholani</h1>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
