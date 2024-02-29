4d18e19ee099c57066152ab958b4483c06d0e62f
Ayda Sholani <ayda.sholani@gmail.com>
2024-02-29 20:58:15 +0100

    first version of header with hero-image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
+import avatar from '../img/avatar.png'
+
 
 export default function Header() {
   return (
     <header>
-      <img 
-        src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Gray01&eyeType=Happy&eyebrowType=Default&mouthType=Smile&skinColor=Light'
-        alt='Avatar' 
-      />
-      <h1>Ayda Sholani</h1>
+      <div 
+        className='hero-image'
+      >
+        <div class="hero-text">
+          <h1>Ayda Sholani</h1>
 
-      <section>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
 
-    <section>
-
-    </section>
+          <button>Contact me</button>
+        </div>
+      </div>
     </header>
   )
 }
