611f952a4782d1cf26d13ef27cbf2bdf4467de47
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-02 18:40:23 +0100

    add all sections to home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import Skills from './Skills'
+import Work from './Work'
 
 export default function Home() {
   return (
     <>
-      Home
+      <section id='about'>
+        <About />
+      </section>
+
+      <section id='skills'>
+        <Skills />
+      </section>
+
+      <section id='work'>
+        <Work />
+      </section>
+
+      <section id='contact'>
+        <Contact />
+      </section>
     </>
   )
 }
