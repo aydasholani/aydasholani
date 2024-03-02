efd2237d996e363938e14e6c521a78b4f78af93c
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-02 18:38:54 +0100

    add hashlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+
+// import { HashLink } from "react-router-dom"
 import SocialsIcons from '../icons/SocialsIcons'
 import Logo from '../logo.png'
+import { HashLink } from 'react-router-hash-link';
 
 export default function Navbar() {
   return (
     <nav>
       <div>
-      <Link>
-        <img src={Logo} alt="logo" height={50} />
-      </Link>
+        <HashLink smooth to="#">
+          <img src={Logo} alt="logo" height={50} />
+        </HashLink>
       </div>
 
       <div className='nav-links'>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/work">work</Link>
-        <Link to="/contact">contact</Link>
+        <HashLink smooth to="#">
+          Home
+        </HashLink>
+        <HashLink to="#about" 
+          scroll={(e) => e.scrollIntoView({ behavior: 'smooth', block: 'end' })}
+        >
+          About
+        </HashLink>
+        <HashLink to="#skills"
+          scroll={(e) => e.scrollIntoView({ behavior: 'smooth', block: 'end' })}
+        >
+          Skills
+        </HashLink>
+        <HashLink to="#work"
+          scroll={(e) => e.scrollIntoView({ behavior: 'smooth', block: 'end' })}
+        >
+          work  
+        </HashLink>
+        <HashLink 
+          to="#contact"
+          scroll={(e) => e.scrollIntoView({ behavior: 'smooth', block: 'end' })}
+        >contact</HashLink>
       </div>
 
       <SocialsIcons />
