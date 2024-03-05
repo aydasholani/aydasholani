08bd312908d9ed08cc7f89138954d309e1157942
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-05 12:11:27 +0100

    add hamburger meny icon

---
+++ src/icons/HamburgerMeny.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+
+export default function HamburgerMeny() {
+  return (
+    <>
+     <AiOutlineMenu size={25} title='Menu'/>
+    </>
+  )
+}
