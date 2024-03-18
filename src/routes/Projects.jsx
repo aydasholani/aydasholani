a042441f05e017786e83133f35683ac045561e96
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-18 20:22:16 +0100

    test layout for projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -4,11 +4,19 @@ export default function Projects() {
   return (
     <>
     <div className="container text-center">
-      
       <header>
         <h2>Projects</h2>
       </header>
-
+      <div className="row">
+        <div className="modal-body">
+          <div className="card text-bg-primary mb-3">
+          <div className="card-header">Header</div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     </>
   )
