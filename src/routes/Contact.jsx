6ae5bc9f1d29c6310c77af72fe8c6d708e25d684
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-18 15:15:43 +0100

    add row and columsn for contact section

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,43 +2,13 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <>
-      {/* <div className="contact-form-wrapper"> */}
+    
       <div className="container text-center">
-        <div className="container">
-          <div className="contact-container">
-            <h3>Contact me</h3>
-          </div>
-          
-          <div className="form-wrapper">
-            <form action="">
-              <label htmlFor="fullname">
-                Name:
-                <input placeholder="Jane Doe" type="text" name="fullname" id="fullname" />
-              </label>
-
-              <label htmlFor="email">
-                Email:
-                <input placeholder="example@email.com" type="email" name="email" id="email" />
-              </label>
-
-              <label htmlFor="message">
-                Message:
-                <textarea
-                  name="message"
-                  id="message"
-                  cols="30"
-                  rows="10"
-                  wrap="off"
-                  placeholder="Write your message here..."
-                ></textarea>
-              </label>
-
-              <input className="btn primary" type="submit" value="Send" />
-            </form>
-          </div>
+        <div className="row">
+          <div className="col-md-6">col1</div>
+          <div className="col-md-6">col2</div>
         </div>
       </div>
-    </>
+
   );
 }
