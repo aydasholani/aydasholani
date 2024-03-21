b99bf412bc0756129565c98b63d153e670128dac
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-21 18:35:22 +0100

    layout for section hobbies  added

--- src/routes/Hobbies.jsx
+++ src/routes/Hobbies.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export default function Hobbies() {
   return (
-    <>
-    <div className="container text-center">
-
-      <header>
+    <div className='d-flex align-items-center justify-content-center 100vh'>
+      <Container className="p-4">
         <h2>Hobbies</h2>
-      </header>
+      </Container>
     </div>
-    </>
   )
 }
