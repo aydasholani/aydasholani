ed795b04fff9a43d7551917ebf8536e23bb6914f
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-21 18:34:53 +0100

    cards for my projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,23 +1,13 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import MyProjects from '../components/MyProjects'
 
 export default function Projects() {
   return (
-    <>
-    <div className="container text-center">
-      <header>
+    <div className='d-flex align-items-center justify-content-center 100vh'>
+      <Container className="p-4">
         <h2>Projects</h2>
-      </header>
-      <div className="row">
-        <div className="modal-body">
-          <div className="card text-bg-primary mb-3">
-          <div className="card-header">Header</div>
-            <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <MyProjects />
+      </Container>
     </div>
-    </>
   )
 }
