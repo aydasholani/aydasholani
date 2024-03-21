bf6fd85ffbe2a6b95a5d0135e8f187d0569d4968
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-21 18:34:38 +0100

    cards for my skills

--- src/routes/Skills.jsx
+++ src/routes/Skills.jsx
@@ -1,89 +1,14 @@
-import React from 'react'
-import { FaReact } from "react-icons/fa";
-import { FaVuejs } from "react-icons/fa";
-import { TbBrandReactNative } from "react-icons/tb";
-import { FaCss3Alt } from "react-icons/fa6";
-import { BsFiletypeScss } from "react-icons/bs";
-import { TbBrandJavascript } from "react-icons/tb";
-import { TbBrandHtml5 } from "react-icons/tb";
-import { TbBrandPython } from "react-icons/tb";
-import { SiSqlalchemy } from "react-icons/si";
-import { TbSql } from "react-icons/tb";
-import { TbBrandNodejs } from "react-icons/tb";
+import { Container } from 'react-bootstrap';
+import MySkills from '../components/MySkills';
 
 export default function Skills() {
-  const mySkills = [
-    {
-      name: "html",
-      icon: <TbBrandHtml5 />
-    },
-    {
-      name: "css",
-      icon: <FaCss3Alt />
-    }, 
-    {
-      name: "scss",
-      icon: <BsFiletypeScss />
-    },
-    {
-      name: "reactjs",
-      icon: <FaReact />
-    },
-    {
-      name: "React native",
-      icon: <TbBrandReactNative />
-    },
-    {
-      name: "vuejs",
-      icon: <FaVuejs />    
-    },
-    {
-      name: "javascript",
-      icon: <TbBrandJavascript />
-    },
-    {
-      name: "flask",
-      icon: <TbBrandJavascript />
-    },
-    {
-      name: "python",
-      icon: <TbBrandPython />
-    },
-    {
-      name: "sql-alchemy",
-      icon: <SiSqlalchemy />
-    },
-    {
-      name: "sql",
-      icon: <TbSql />
-    },
-    {
-      name: "nodejs",
-      icon: <TbBrandNodejs />
-    }
-  ]
-
-  const displaySkills = mySkills.map((skill) => {
-    return (
-      <div className='col-6' key={skill.name}>
-        <div className="p-3 bg-dark text-white">
-          {skill.icon} {skill.name}
-        
-        </div>
-      </div>
-    )
-  })
 
   return (
-    <>
-    <div className="container text-center">
-    <header>
-      <h2>Skills</h2>
-    </header>
-      <div className='skills row g-4'>
-        {displaySkills}
-      </div>
+    <div className='d-flex align-items-center justify-content-center 100vh'>
+      <Container className="p-4">
+        <h2>Skills</h2>
+        <MySkills />
+      </Container>
     </div>
-    </>
   )
 }
