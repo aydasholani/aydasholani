831a2b0da95fc11552e7db8b062e8a1218b467de
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-21 18:36:05 +0100

    add new layout for landingpage

--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -2,23 +2,30 @@
 import Skills from './Skills'
 import Projects from './Projects'
 import Hobbies from './Hobbies'
+import Testimonials from './Testimonials';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function LandingPage() {
   return (
-    <>
-      <main>
-        <section id='skills'>
-          <Skills />
-        </section>
+    <Container fluid className='main-content p-0 gx-0'>
+        <Row>
+          <Col xs={12} id="skills" className='p-5'>
+            <Skills />
+          </Col>
 
-        <section id='projects'>
+          <Col xs={12} id="projects" className='p-5'>
           <Projects />
-        </section>
+          </Col>
 
-        <section id='hobbies'>
+          <Col xs={12} id="testimonials" className='p-5'>
+            <Testimonials />
+          </Col>
+
+          <Col xs={12} id="hobbies" className='p-5'>
           <Hobbies />
-        </section>
-      </main>
-    </>
+          </Col>
+        </Row>
+    </Container>
+
   )
 }
