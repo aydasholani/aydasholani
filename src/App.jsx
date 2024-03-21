3958803ea5b807cb7de127d752c5bc7a928a4322
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-21 14:20:58 +0100

    add theme provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import LandingPage from './routes/LandingPage';
-
+import { ThemeProvider } from 'react-bootstrap';
 function App() {
   return (
-    <>
+    <ThemeProvider
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+
       <Header />
       <LandingPage />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
