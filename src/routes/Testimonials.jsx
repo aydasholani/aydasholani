4840db37214fe98d9aea3e3d45f4c1f1627e88fc
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-25 21:14:59 +0100

    new design for testimonials

--- src/routes/Testimonials.jsx
+++ src/routes/Testimonials.jsx
@@ -5,25 +5,25 @@ export default function Testimonials() {
     <section className='d-flex align-items-center justify-content-center 100vh'>
     <Container className="p-4">
       <h2>Testimonials</h2>
-      <Row className="p-4">
+      <Row className="g-2">
 
-        <Carousel>
+        <Carousel className='testimonials'>
           <Carousel.Item interval={5000} >
-            <img src="https://picsum.photos/200/100?random=1" alt="p1" />
+            <img src="https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="p1" />
             <Carousel.Caption>
               <h3>Person 1</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={5000} >
-          <img src="https://picsum.photos/200/100?random=2" alt="p2" />
+          <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  alt="p2" />
             <Carousel.Caption>
               <h3>Person 2</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item interval={5000} >
-          <img src="https://picsum.photos/200/100?random=3" alt="p3" />
+          <img src="https://images.unsplash.com/photo-1510511459019-5dda7724fd87?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="p3" />
             <Carousel.Caption>
               <h3>Person 3</h3>
               <p>
