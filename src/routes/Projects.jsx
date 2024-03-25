ae282b5d4c67c09aa3d45e08b8b1634d73e61e6c
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-25 16:47:51 +0100

    refactor projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -11,17 +11,17 @@ export default function Projects() {
 
   const displayProjects = myProjects.map((project, index) => {
     return (  
-      <Col key={project.name}  xxs={6} md={4} lg={4} xl={3} className="p-2">
-      <Card bg="dark" text="white" className="project-card">
-      <Card.Header>{project.name}</Card.Header>
-        <Card.Body>
-          <Card.Img src={`https://picsum.photos/200/100?random=${index}`} alt={project.name} />
-          <Card.Title>{project.name}</Card.Title>
-          <Card.Text>{project.description}</Card.Text>
-          <Card.Link href={project.link} target="_blank">GitHub-Link</Card.Link>
-        </Card.Body>
-      </Card>
-    </Col>
+      <Col key={project.name}  xxs={6} md={4} lg={4} xl={4} className="p-2">
+        <Card bg="dark" text="white" className="project-card">
+        <Card.Header>{project.name}</Card.Header>
+          <Card.Body>
+            <Card.Img src={`https://picsum.photos/200/100?random=${index}`} alt={project.name} />
+            <Card.Title>{project.name}</Card.Title>
+            <Card.Text>{project.description}</Card.Text>
+            <Card.Link href={project.link} target="_blank">GitHub-Link</Card.Link>
+          </Card.Body>
+        </Card>
+      </Col>
     )
   })
   return (
