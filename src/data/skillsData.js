dfaee70df75a3d27f6b8a5c831672c54d06afe20
Ayda Sholani <ayda.sholani@gmail.com>
2024-03-29 12:26:52 +0100

    new skill data

---
+++ src/data/skillsData.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    "key": 1,
+    "name": "git",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+  },
+  {
+    "key": 2,
+    "name": "html",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+  },
+  {
+    "key": 3,
+    "name": "css",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+  }, 
+  {
+    "key": 4,
+    "name": "scss",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg"
+  },
+  {
+    "key": 9,
+    "name": "javascript",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+  },
+  {
+    "key": 5,
+    "name": "reactjs",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+  },
+  {
+    "key": 6,
+    "name": "react-router-dom",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactrouter/reactrouter-original.svg"
+  },
+  {
+    "key": 7,
+    "name": "react-bootstrap",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/reactbootstrap/reactbootstrap-original.svg"
+  },
+  {
+    "key": 8,
+    "name": "vuejs",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg"    
+  },
+  {
+    "key": 11,
+    "name": "python",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+  },
+  {
+    "key": 12,
+    "name": "sqlite",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg"
+  },
+  {
+    "key": 13,
+    "name": "sql-alchemy",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlalchemy/sqlalchemy-original.svg"
+  },
+  {
+    "key": 10,
+    "name": "flask",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg"
+  },
+  {
+    "key": 14,
+    "name": "nodejs",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
+  },
+  {
+    "key": 15,
+    "name": "npm",
+    "img": "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/npm/npm-original-wordmark.svg"
+  }
+]
