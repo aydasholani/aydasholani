7e0939b008382d09f1ed5e884e379a7451919004
Ayda Sholani <ayda.sholani@gmail.com>
2024-04-01 14:35:05 +0200

    center button

--- src/components/Weather/FetchWeatherData.jsx
+++ src/components/Weather/FetchWeatherData.jsx
@@ -29,27 +29,24 @@ export default function FetchWeatherData(props) {
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData, lon, lat]);
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     getCurrentPosition()
   }
-
+  
   const fetchData = async () => {
     console.log(WEATHER_API_URL)
     try {
       const weatherResponse = await fetch(WEATHER_API_URL)
       
       const weatherData = await weatherResponse.json();
-
+      
       if (weatherData.cod === '404') {
         props.setDataLoaded(false);
         return;
       }
-     
+      
       props.setData({
         name: weatherData.name,
         temp: Math.ceil(weatherData.main.temp),
@@ -59,15 +56,18 @@ export default function FetchWeatherData(props) {
         icon: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
       })
       console.log(weatherData)
-
+      
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, lon, lat]);
 
   return (
     <div>
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} className='d-flex justify-content-center align-items-center'>
       <Row xs={6}>
         <Col xs="auto">
           <Button variant="outline-primary" type="submit" className='mt-2'>
